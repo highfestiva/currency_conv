53df2fb0bd98b3b6fccb42f05e113601c6ce3970
Jonas Bystrom <jonas.bystrom@hm.com>
2017-06-19 11:22:12 +0200

    Clean up time-of-update code
    Also converted tabs to spaces.

--- currency_conv.py
+++ currency_conv.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import requests
 import time
 import urllib.request
@@ -15,33 +16,33 @@ refresh_threshold = 24*60*60
 
 
 def load():
-	try:
-		r = requests.get(ecb_url, proxies=urllib.request.getproxies())
-		if r.status_code == 200:
-			with open(filename, 'wb') as f:
-				f.write(r.content)
-				global updated
-				updated = time.time()
-	except Exception as e:
-		print(e)
-		pass	# Better luck next time.
+    try:
+        r = requests.get(ecb_url, proxies=urllib.request.getproxies())
+        if r.status_code == 200:
+            with open(filename, 'wb') as f:
+                f.write(r.content)
+    except Exception as e:
+        print(e)
+        pass    # Better luck next time.
 
-	with ZipFile(filename, 'r') as z:
-		csv = z.read(csvname).decode()
-	lines = csv.strip().splitlines()
-	currencies = [w.strip() for w in lines[0].split(',')[1:] if w.strip()]
-	rates = [float(w.strip()) for w in lines[1].split(',')[1:] if w.strip()]
-	for c,r in zip(currencies,rates):
-		cur2eur[c] = r
+    with ZipFile(filename, 'r') as z:
+        csv = z.read(csvname).decode()
+    lines = csv.strip().splitlines()
+    currencies = [w.strip() for w in lines[0].split(',')[1:] if w.strip()]
+    rates = [float(w.strip()) for w in lines[1].split(',')[1:] if w.strip()]
+    for c,r in zip(currencies,rates):
+        cur2eur[c] = r
+    global updated
+    updated = os.stat(filename).st_mtime
 
 
 def convert(amount, from_currency, to_currency):
-	if not cur2eur or time.time()-updated > refresh_threshold:
-		load()
-	eur = amount
-	if from_currency != 'EUR':
-		eur = amount / cur2eur[from_currency]
-	res = eur
-	if to_currency != 'EUR':
-		res = eur * cur2eur[to_currency]
-	return res
+    if time.time()-updated > refresh_threshold:
+        load()
+    eur = amount
+    if from_currency != 'EUR':
+        eur = amount / cur2eur[from_currency]
+    res = eur
+    if to_currency != 'EUR':
+        res = eur * cur2eur[to_currency]
+    return res
